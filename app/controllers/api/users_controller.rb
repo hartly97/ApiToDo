a6c93a7817a8618ff3db986b217b19cfcd28e459
hartly97 <mhartwikitree@gmail.com>
2017-08-24 10:32:47 -0500

    create:add create to user ctrl

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -8,11 +8,20 @@ class Api::UsersController < ApiController
   end
 
   def create
+    user = User.new(user_params)
+     if user.save
+       # # 5 - look for a serializer named UserSerializer and use it to serialize user.
+       render json: user
+     else
+       # # 6 - If saving the user fails (due to a missing user name or password)
+       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+     end
+
    end
 
    private
    def user_params
      params.require(:user).permit(:username, :password)
    end
-   
+
 end
